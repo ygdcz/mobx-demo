c44423c2afe35f254142dedde77d225c7087ef81
曹荻 <caodi@datagrand.com>
2022-04-02 11:54:35 +0800

    feat(Home)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,6 +13,7 @@ const Home = () => {
       <div>
         <SkypeOutlined style={{ fontSize: 70, color: 'red' }} />
         <span className={styles['text']}>生鲜超市</span>
+        <span>游客点击购买，指引去登陆</span>
       </div>
       <img src='https://img1.baidu.com/it/u=1134622122,710615167&fm=253&fmt=auto&app=138&f=JPEG?w=741&h=445' />
     </div>
